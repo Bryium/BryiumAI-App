d4edd04910a99f72b771be31d68588b3c207b8b0
Bryium <bryiumonyancha@gmail.com>
2025-11-19 20:14:09 +0300

    add the taiwind styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native";
+import { Text, View } from "react-native";
 
-export default function Example() {
+export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-semibold text-blue-500">
-        Hello Tailwind
+      <Text className="text-xl font-bold text-blue-500">
+        My name is Bryium Onyancha and I am a software engineer working at
+        Google. I have a masters degree in computer science from William Jessup
+        University.
       </Text>
     </View>
   );
